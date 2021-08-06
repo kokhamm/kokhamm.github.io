1cf8b96987962ed9a150e040cbbc752aa8ec572a
kokhamm <88533798+kokhamm@users.noreply.github.com>
2021-08-06 11:46:44 +0300

    upload my homework
    how to make milion

---
+++ javascript/main.js
@@ -0,0 +1,3 @@
+var bg = document.getElementById('form');
+document.getElementById('btnfix').addEventListener("mouseover", function(){bg.style.background = "#c00";this.addEventListener("mouseout", function(){bg.style.background = "";});
+});
